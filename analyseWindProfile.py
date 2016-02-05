ce9fe47ee1e7dd68258ef8544770d2d8818ed0d9
jmccormac01 <jmccormac001@gmail.com>
2016-02-05 14:48:08 +0000

    initial commit of analyseWindProfile

---
+++ analyseWindProfile.py
@@ -0,0 +1,28 @@
+#!/usr/local/python/bin/python
+#
+# script to plot the CDF wind speed at paranal
+#
+
+import pymysql
+import datetime as dt
+import matplotlib.pyplot as pl
+import numpy as np
+
+t2=dt.datetime.utcnow()
+t1=t2-dt.timedelta(days=365)
+
+db=pymysql.connect(host='ds',db='ngts_ops')
+qry="SELECT wind_speed_avg,wind_speed_max FROM weather_log WHERE tsample between \'%s\' and \'%s\'" % (t1,t2)
+avg,mx=[],[]
+with db.cursor() as cur:
+	cur.execute(qry)
+	for row in cur:
+		avg.append(row[0])
+		mx.append(row[1])
+avg=np.array(avg)
+mx=np.array(mx)
+
+for i in range(10,25):
+	n_avg=np.where(avg>i)
+	n_mx=np.where(mx>i)
+	print "Limit: %d m/s - Average > Limit: %d - Max > Limit: %d" % (i,len(n_avg[0]),len(n_mx[0]))
