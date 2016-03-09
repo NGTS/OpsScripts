ac570892afd9e1fcc404dceb3914d82449a08390
jmccormac01 <jmccormac001@gmail.com>
2016-03-09 21:36:54 +0000

    adding action_id argument

--- analyseMountSkipping.py
+++ analyseMountSkipping.py
@@ -1,6 +1,12 @@
 #!/usr/local/python/bin/python
 import pymysql
 import numpy as np
+import argparse as ap
+
+def argParse():
+	parser=ap.ArgumentParser(description="Script to analyse the mount skipping frequency")
+	parser.add_argument('action_id',type=int,help="Action ID to analyse skipping frequency")
+	return parser.parse_args()
 
 db=pymysql.connect(host='ds',db='ngts_ops')
 qry="SELECT x,y FROM autoguider_log WHERE action_id=%d" % (action_id)
