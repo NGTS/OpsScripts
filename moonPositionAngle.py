50111730965dc3b613cb98079ed50e735cea4a9c
jmccormac01 <jmccormac001@gmail.com>
2016-03-17 11:26:15 +0000

    measuring position angle

--- moonPositionAngle.py
+++ moonPositionAngle.py
@@ -9,12 +9,18 @@ db=pymysql.connect(host='ngtsdb',db='ngts_ops')
 action_ids=np.array([107845,107922,108010])
 
 for i in action_ids:
-	cmd_ra,cmd_dec,moon_ra,moon_dec=[],[],[],[]
+	position_angle=[]
 	qry="SELECT cmd_ra,cmd_dec,moon_ra,moon_dec FROM raw_image_list WHERE action_id=%d" % (i)
 	with db.cursor() as cur:
 		cur.execute(qry)
 		for row in cur:
-			cmd_ra.append(row[0])
-			cmd_dec.append(row[1])
-			moon_ra.append(row[2])
-			moon_dec.append(row[3])
+			cmd_ra=row[0]
+			cmd_dec=row[1]
+			moon_ra=row[2]
+			moon_dec=row[3]
+			obj=SkyCoord(cmd_ra*u.deg,cmd_dec*u.deg,frame='icrs')
+			moon=SkyCoord(moon_ra*u.deg,moon_dec*u.deg,frame='icrs')
+			position_angle.append(obj.position_angle(moon))
+
+
+
