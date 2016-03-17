af858d983511ee34065af2e176135482daf0dac8
jmccormac01 <jmccormac001@gmail.com>
2016-03-17 11:00:01 +0000

    initial commit of moonPositionAngle script

---
+++ moonPositionAngle.py
@@ -0,0 +1,20 @@
+#!/usr/local/python/bin/python
+# script to measure the position angle to the moon
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+import numpy as np
+import pymysql
+
+db=pymysql.connection(host='ngts_db',db='ngts_ops')
+action_ids=np.array([107845,107922,108010])
+
+for i in action_ids:
+	qry="SELECT cmd_ra,cmd_dec,moon_ra,moon_dec FROM raw_image_list WHERE action_id=%d" % (i)
+	with db.cursor() as cur:
+		cur.execute(qry)
+		for row in cur:
+			cmd_ra.append(row[0])
+			cmd_dec.append(row[1])
+			moon_ra.append(row[2])
+			moon_dec.append(row[3])
+	
