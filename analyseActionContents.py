d6f062bb6fcb28c2760e68e2f84e9dfcc963bf93
James McCormac <jmccormac001@gmail.com>
2016-07-03 10:10:36 +0100

    bug fix cnts --> cts

--- analyseActionContents.py
+++ analyseActionContents.py
@@ -4,7 +4,7 @@ db =  pymysql.connect(host='ds', db='ngts_ops')
 actions = np.arange(124659,124874,1)
 for i in actions:
     with db.cursor() as cur:
-        qry = 'SELECT img_median_cnts FROM raw_image_list WHERE action_id={0:d}'.format(i)
+        qry = 'SELECT img_median_cts FROM raw_image_list WHERE action_id={0:d}'.format(i)
         cur.execute(qry)
         meds = []
         for row in cur:
