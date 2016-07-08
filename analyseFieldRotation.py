57301c8c91a3b109e037608e2c7882bd20059da4
James McCormac <jmccormac001@gmail.com>
2016-07-08 18:41:31 +0100

    adding some plotting ideas

--- analyseFieldRotation.py
+++ analyseFieldRotation.py
@@ -13,7 +13,13 @@ Questions:
     1. Have the reference images been solved with WCS fit?
     2. Do we have a list of apertures from the Transparency code?
     3. Do the findings match up with the Transparency issue?
+
+To do:
+    1. Get the time for each image for plotting
+    2. Make a 2x2 grid of plots for ROTATION defined by
+       each of the 4 CD matrix coeffs
 """
+import math
 import pymysql
 from collections import defaultdict
 import numpy as np
@@ -21,15 +27,18 @@ import matplotlib.pyplot as plt
 from astropy.time import Time
 import astropy.units as u
 
-
 # connect to the different dbs
 ops_db = pymysql.connect(host='ngtsdb', db='ngts_ops')
 pipe_db = pymysql.connect(host='ngtsdb', db='ngts_pipe')
+
+# globals
 field_id = 'NG2100-4748'
+cdelta = 4.98
 
 # store results here
 action_ids, night = [], []
 image_ids = defaultdict(list)
+start_times = defaultdict(list)
 cd_matrix = defaultdict(list)
 
 ops_qry = """SELECT
@@ -51,7 +60,7 @@ print("Found {0:d} actions for {1:s}".format(len(action_ids), field_id))
 # now loop over all the actions and get the image_ids and WCS info
 for action in action_ids:
     image_qry = """SELECT
-                image_id
+                image_id, start_time_utc
                 FROM
                 raw_image_list
                 WHERE
@@ -62,7 +71,7 @@ for action in action_ids:
         ops_cur.execute(image_qry)
         for row in ops_cur:
             image_ids[action].append(row[0])
-
+            start_times[action].append(row[1])
             pipe_qry = """SELECT
                         cd1_1, cd1_2, cd2_1, cd2_2
                         FROM
@@ -78,3 +87,25 @@ for action in action_ids:
 
     # stack the CD arrays into one nice numpy array
     cd_matrix[action] = np.vstack(cd_matrix[action])
+
+# set up a figure for plotting
+#fig=plt.figure(1,figsize=(15,15))
+#ax11=plt.subplot2grid((4,4),(0,0),colspan=2,rowspan=2)
+#ax12=plt.subplot2grid((4,4),(0,2),colspan=2,rowspan=2)
+#ax21=plt.subplot2grid((4,4),(2,0),colspan=2,rowspan=2)
+#ax22=plt.subplot2grid((4,4),(2,2),colspan=2,rowspan=2)
+#
+#for action in cd_matrix:
+#    rot1_1 = math.acos(cd_matrix[action][0]/cdelta)
+#    rot1_2 = math.asin(cd_matrix[action][1]/cdelta)
+#    rot2_1 = math.asin(cd_matrix[action][2]/cdelta)
+#    rot2_2 = math.acos(cd_matrix[action][3]/cdelta)
+#
+#    # not plot all the rotations in a 2x2 grid
+#    # 1 rot calc per grid, showing all values over time
+#    ax11.plot(start_times[action], rot1_1, 'k.')
+#    ax12.plot(start_times[action], rot1_2, 'k.')
+#    ax21.plot(start_times[action], rot2_1, 'k.')
+#    ax22.plot(start_times[action], rot2_2, 'k.')
+#
+#plt.show()
