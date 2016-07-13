b22a0544320ab15029e71fe068309761db7f5e60
James McCormac <jmccormac001@gmail.com>
2016-07-13 12:02:12 +0100

    fixing bug in time correction

--- analyseFieldRotation.py
+++ analyseFieldRotation.py
@@ -1,23 +1,7 @@
 #!/usr/local/python/bin/python
 """
-Code to investigate the field rotation spotted between
-the PRESURVEY images and the the AG reference image
-
-Process:
-    1. Start by getting the reference image - are they solved?
-    2. Get a sample of images throughout the season
-    3. Display them to compare them for rotation
-    4. Check the WCS solutions for rotation
-
-Questions:
-    1. Have the reference images been solved with WCS fit?
-    2. Do we have a list of apertures from the Transparency code?
-    3. Do the findings match up with the Transparency issue?
-
-To do:
-    1. Get the time for each image for plotting
-    2. Make a 2x2 grid of plots for ROTATION defined by
-       each of the 4 CD matrix coeffs
+Code to grab the CD matrices from a field's
+images and look for rotation
 """
 import math
 import pymysql
@@ -26,6 +10,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from astropy.time import Time
 import astropy.units as u
+
+def fitQuad(x, y):
+    """
+    Generic function to fit 2D polynomial
+    to time series data
+    """
+    xn = np.linspace(x.min(), x.max(), 100)
+    coeffs = np.polyfit(x,y,2)
+    besty = np.polyval(coeffs, xn)
+    return xn, besty
+
+def getSurveyFields():
+    """
+    Code to grab all the survey fields to check for rotation
+    """
+    pass
 
 # connect to the different dbs
 db = pymysql.connect(host='ngtsdb', db='ngts_ops')
@@ -78,6 +78,7 @@ with db.cursor() as cur:
             cd22.append(row[5])
 
 # make numpy arrays
+time = np.array(times)
 cd11 = np.array(cd11)
 cd12 = np.array(cd12)
 cd21 = np.array(cd21)
@@ -86,26 +87,34 @@ cd22 = np.array(cd22)
 # make the x axis easier to plot
 times = times - times[0]
 
+# work out the rotation from the CD matrices
+rot11 = np.arccos(np.radians(cd11/cdelta))
+rot12 = np.arcsin(np.radians(cd12/cdelta))
+rot21 = np.arcsin(np.radians(cd21/cdelta))
+rot22 = np.arccos(np.radians(cd22/cdelta))
+
+# fit the data
+tcd11, cd11_fit = fitQuad(times, rot11)
+tcd12, cd12_fit = fitQuad(times, rot12)
+tcd21, cd21_fit = fitQuad(times, rot21)
+tcd22, cd22_fit = fitQuad(times, rot22)
+
 # set up a figure for plotting
 fig=plt.figure(1,figsize=(15,15))
 ax11=plt.subplot2grid((4,4),(0,0),colspan=2,rowspan=2)
 ax12=plt.subplot2grid((4,4),(0,2),colspan=2,rowspan=2, sharex=ax11)
 ax21=plt.subplot2grid((4,4),(2,0),colspan=2,rowspan=2, sharex=ax11)
 ax22=plt.subplot2grid((4,4),(2,2),colspan=2,rowspan=2, sharex=ax11)
-rot11 = np.arccos(np.radians(cd11/cdelta))
-rot12 = np.arcsin(np.radians(cd12/cdelta))
-rot21 = np.arcsin(np.radians(cd21/cdelta))
-rot22 = np.arccos(np.radians(cd22/cdelta))
-ax11.plot(times, rot11/np.median(rot11), 'k.')
+ax11.plot(times, rot11/np.median(rot11), 'k.', tcd11, cd11_fit, 'r-')
 ax11.set_title('CD1_1 Rot')
 ax11.set_ylabel('Rot / median(rot)')
-ax12.plot(times, rot12/np.median(rot12), 'k.')
+ax12.plot(times, rot12/np.median(rot12), 'k.', tcd12, cd12_fit, 'r-')
 ax12.set_title('CD1_2 Rot')
-ax21.plot(times, rot21/np.median(rot21), 'k.')
+ax21.plot(times, rot21/np.median(rot21), 'k.', tcd21, cd21_fit, 'r-')
 ax21.set_title('CD2_1 Rot')
 ax21.set_ylabel('Rot / median(rot)')
 ax21.set_xlabel('Days since day 0')
-ax22.plot(times, rot22/np.median(rot22), 'k.')
+ax22.plot(times, rot22/np.median(rot22), 'k.', tcd22, cd22_fit, 'r-')
 ax22.set_title('CD2_2 Rot')
 ax22.set_xlabel('Days since day 0')
 plt.show()
