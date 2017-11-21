619bf6b2c93c96ab80a44df1c8e9aa6ad2558e80
James McCormac <jmccormac001@gmail.com>
2017-11-21 11:18:09 +0000

    adding a cut on the errors for bad nights

--- getAgSeasonRms.py
+++ getAgSeasonRms.py
@@ -11,10 +11,10 @@ if __name__ == "__main__":
     refs = ['80520150922062822', '80520160114005454',
             '80520160114005520', '80520160316004726']
     qry = """
-        SELECT x_error, y_error
+        SELECT x_error, y_error, quality
         FROM autoguider_log
-        WHERE ref_image_id IN
-            ({})
+        WHERE ref_image_id IN ({})
+        AND ABS(x_error) < 10 and ABS(y_error) < 10
         """.format(','.join(refs))
     with pymysql.connect(host='10.2.5.32', db='ngts_ops') as cur:
         cur.execute(qry)
