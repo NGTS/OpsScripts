d0f12a5eb5e932e506971f7438884e53c7af3418
James McCormac <jmccormac001@gmail.com>
2017-11-21 11:09:19 +0000

    fixing qry - cannot pass arg as part of query struct

--- getAgSeasonRms.py
+++ getAgSeasonRms.py
@@ -6,16 +6,16 @@ import pymysql
 import numpy as np
 
 if __name__ == "__main__":
-    refs = [80520150922062822, 80520160114005454,
-            80520160114005520, 80520160316004726]
+    refs = ['80520150922062822', '80520160114005454',
+            '80520160114005520', '80520160316004726']
     qry = """
         SELECT x_error, y_error
         FROM autoguider_log
         WHERE ref_image_id IN
-            (%s)
-        """
+            ({})
+        """.format(','.join(refs))
     with pymysql.connect(host='10.2.5.32', db='ngts_ops') as cur:
-        cur.execute(qry, (','.join(refs)))
+        cur.execute(qry)
         results = cur.fetchall()
     x_error = np.zeros(len(results))
     y_error = np.zeros(len(results))
