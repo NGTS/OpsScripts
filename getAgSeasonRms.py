cdae5a70cf0afb1aacd8b62535141934c38a0b31
James McCormac <jmccormac001@gmail.com>
2018-03-07 20:22:34 +0000

    adding mult-target support to getAgSeasonRms.py

--- getAgSeasonRms.py
+++ getAgSeasonRms.py
@@ -2,21 +2,36 @@
 Get the season RMS using the image IDs of the reference
 images as a cross match
 """
+import argparse as ap
 import pymysql
 import numpy as np
 
 # pylint: disable=invalid-name
 
+pix_scale = 4.96
+refs = {'NGTS-4b': ['80520150922062822', '80520160114005454',
+                   '80520160114005520', '80520160316004726'],
+        'NGTS-3Ab': ['81220170204024257', '81220160819092321', 
+                     '81220170130033545']}
+
+def argParse(refs):
+    """
+    parse the command line arguments
+    """
+    p = ap.ArgumentParser()
+    p.add_argument('target',
+                   help='target ID',
+                   choices=refs.keys())
+    return p.parse_args()
+    
 if __name__ == "__main__":
-    pix_scale = 4.96
-    refs = ['80520150922062822', '80520160114005454',
-            '80520160114005520', '80520160316004726']
+    args = argParse()
     qry = """
         SELECT x_error, y_error, quality
         FROM autoguider_log
         WHERE ref_image_id IN ({})
         AND ABS(x_error) < 50 and ABS(y_error) < 50
-        """.format(','.join(refs))
+        """.format(','.join(refs[args.target]))
     with pymysql.connect(host='10.2.5.32', db='ngts_ops') as cur:
         cur.execute(qry)
         results = cur.fetchall()
