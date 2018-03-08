466d26c0a7000cb65080694d790c94f714fccca3
James McCormac <jmccormac001@gmail.com>
2018-03-08 13:51:58 +0000

    list comprehensions!!!!

--- syncAgRefsFromParanal.py
+++ syncAgRefsFromParanal.py
@@ -12,6 +12,6 @@ import numpy as np
 
 if __name__ == "__main__":
     os.chdir('/ngts/autoguider_ref/')
-    warwick = np.loadtxt('ags_at_warwick.txt', usecols=[0], unpack=True)
+    warwick = [line.rstrip() for line in open('ags_at_warwick.txt').readlines()]
     paranal = sorted(g.glob('*.fits'))
     sync = [image for image in paranal if image not in warwick]
