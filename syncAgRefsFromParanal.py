c9bd889a41315e47a3d7dbc5bb3fb2d945108533
James McCormac <jmccormac001@gmail.com>
2018-03-08 13:49:44 +0000

    adding sync script to repo

---
+++ syncAgRefsFromParanal.py
@@ -0,0 +1,17 @@
+"""
+The old rsync of archive copied the ag references
+This was stopped and now we must manually sync them
+back occassionally. This script works out which need
+synced back
+"""
+import os
+import glob as g
+import numpy as np
+
+# pylint: disable=invalid-name
+
+if __name__ == "__main__":
+    os.chdir('/ngts/autoguider_ref/')
+    warwick = np.loadtxt('ags_at_warwick.txt')
+    paranal = sorted(g.glob('*.fits'))
+    sync = [image for image in paranal if image not in warwick]
