2fa31c6c83ed40d0d49c8a9beb9f958f77597d3e
James McCormac <jmccormac001@gmail.com>
2019-10-17 17:01:10 +0100

    fixes plotting of battery voltages

--- plotRoofBatteries.py
+++ plotRoofBatteries.py
@@ -22,18 +22,20 @@ def get_battery_data(start, finish):
 if __name__ == "__main__":
     start = "2019-01-01 00:00:00"
     finish = "2019-10-31 23:59:59"
-    data = get_battery_data(start, finish)
+    datas = get_battery_data(start, finish)
 
-    time = data['last_update']
-    north_1 = data['north_v2'][::500]
-    north_2 = data['north_v4'][::500]
-    south_1 = data['south_v1'][::500]
-    south_2 = data['south_v4'][::500]
+    time, north_1, north_2, south_1, south_2 = [], [], [], [], []
+    for data in datas:
+        time.append(data['last_update'])
+        north_1.append(data['north_v2'])
+        north_2.append(data['north_v4'])
+        south_1.append(data['south_v1'])
+        south_2.append(data['south_v4'])
 
     fig, ax = plt.subplots(2, figsize=(10, 10), sharex=True)
-    ax[0].plot(time, north_1, 'r-')
-    ax[0].plot(time, north_2, 'k-')
-    ax[1].plot(time, south_1, 'r-')
-    ax[1].plot(time, south_2, 'k-')
+    ax[0].plot(time[::500], north_1[::500], 'r-')
+    ax[0].plot(time[::500], north_2[::500], 'k-')
+    ax[1].plot(time[::500], south_1[::500], 'r-')
+    ax[1].plot(time[::500], south_2[::500], 'k-')
     fig.savefig('battery_voltage.png', dpi=300)
 
